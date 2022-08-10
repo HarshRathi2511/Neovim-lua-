89ceac5a2233261be1147e4ddf98b32c63ece0d8
Harsh Rathi <rathiharsh2002@gmail.com>
2022-08-11 00:22:21 +0530

    some more lsp configs

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -97,7 +97,7 @@ cmp.setup({
 			vim_item.kind = kind_icons[vim_item.kind]
 			vim_item.menu = ({
 				nvim_lsp = "[LSP]",
-				nvim_lua = "[LUA]",
+				nvim_lua = "[Lua]",
 				luasnip = "[Snippet]",
 				buffer = "[Buffer]",
 				path = "[Path]",
