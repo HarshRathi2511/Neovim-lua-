8b4440e7ad33bcec72b7aed24acb9dbb5df3f9fc
Harsh Rathi <rathiharsh2002@gmail.com>
2022-08-13 12:58:18 +0530

    add: prettier to nullls

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -22,6 +22,7 @@ null_ls.setup({
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
 		formatting.google_java_format,
+		formatting.prettier,
 		diagnostics.flake8,
 	},
 	--a hook which autoformats on save
