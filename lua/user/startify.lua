1d4f37228eeb9a9b67add8fd1fe2110ac55a244a
Harsh Rathi <rathiharsh2002@gmail.com>
2022-08-16 19:07:34 +0530

    add:replaced alpha with startify

---
+++ lua/user/startify.lua
@@ -0,0 +1,32 @@
+vim.cmd([[
+"Here all the sessions will be stored 
+let g:startify_session_dir = '~/.config/nvim/session'
+
+let g:startify_lists = [
+          \ { 'type': 'files',     'header': ['   Files']            },
+          \ { 'type': 'dir',       'header': ['   Current Directory '. getcwd()] },
+          \ { 'type': 'bookmarks', 'header': ['   Bookmarks']      },
+          \ { 'type': 'sessions',  'header': ['   Sessions']       },
+          \ ]
+
+let g:startify_bookmarks = [
+            \ { 'c': '~/.config/nvim/init.lua' },
+            \ { 'b': '~/.bashrc' },
+            \ { 'v': '~/Documents/Backend'},
+            \ {'d':'~/Documents/DSA/'},
+            \ {'s':'~/Documents/Organizations/swoop'},
+            \ ]
+
+let g:startify_session_delete_buffers = 1
+let g:startify_change_to_vcs_root = 1
+let g:startify_fortune_use_unicode = 1
+let g:startify_session_persistence = 1
+let g:startify_enable_special = 0
+
+let g:startify_custom_header = [
+        \ '   _  __     _         __  ___         __     ___ ',
+        \ '  / |/ /  __(_)_ _    /  |/  /__ _____/ /    |_  |',
+        \ ' /    / |/ / /  ` \  / /|_/ / _ `/ __/ _ \  / __/ ',
+        \ '/_/|_/|___/_/_/_/_/ /_/  /_/\_,_/\__/_//_/ /____/ ',
+        \]
+]])
